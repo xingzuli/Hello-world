18c06e6260125e0be0aa057cba8b8a07435e8960
xingzuli <xingzuli@sina.com>
2016-08-10 09:20:10 +0800

    tesr new bat

--- quickPush.bat
+++ quickPush.bat
@@ -1,13 +1,16 @@
 @echo off
 echo Input the message of the commmit
 set /p message=
+echo your message is %message%
 git add -A
-echo complete adding the change 
-git commit -m %message%
+echo complete adding the change
+echo.
+git commit -m "%message%"
 echo complete commit
+echo.
 git pull
 echo complete pull
+echo.
 git push
 echo complete push
-pause
-
+echo.
