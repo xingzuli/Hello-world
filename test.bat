670f482aa9c9339af740d0fa5dc7cfdbae2b251f
xingzuli <xingzuli@sina.com>
2016-08-10 09:05:52 +0800

    test

--- test.bat
+++ test.bat
@@ -2,12 +2,12 @@
 echo Input the message of the commmit
 set /p message=
 git add -A
-echo. complete adding the change
-git commit -m %message%
-echo. complete commit
+echo complete adding the change^
+git commit -m "%message%"
+echo complete commit^
 git pull
-echo. complete pull
+echo complete pull^
 git push
-echo complete push
+echo complete push^
 pause
 
